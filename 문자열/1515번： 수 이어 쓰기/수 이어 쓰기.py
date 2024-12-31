d576098a21c83c0be88d3a729138270699f05528
Juhwan01 <wnghks5432@naver.com>
2024-12-31 23:02:48 +0900

    1515-풀이(해설)

--- 문자열/1515번： 수 이어 쓰기/수 이어 쓰기.py
+++ 문자열/1515번： 수 이어 쓰기/수 이어 쓰기.py
@@ -11,34 +11,15 @@
 #  **************************************************************************  #
 import sys
 input = sys.stdin.readline
-
 n = input().rstrip()
-numbers = [int(num) for num in n]
-i=1
-length = len(numbers)
-while i<length:
-    if(numbers[i]>numbers[i-1]):
-        i+=1
-        continue
-    else:
-        j = numbers[i-1]
-        while True:
-            j+=1
-            cnt=0
-            if str(numbers[i]) in str(j):
-                tmp = str(j).replace(str(numbers[i]),'',1)
-                print(tmp)
-                for k in range(i+1,length):
-                    if  str(numbers[k]) in str(j) and cnt<len(str(j)):
-                        tmp=tmp.replace(str(numbers[k]),'',1)
-                        print(tmp)
-                        cnt+=1
-                    else:
-                        break
-                del numbers[i+1:i+1+cnt]
-                numbers[i] = j
-                length -= cnt
-                i+=1
-                break
-print(numbers)
-print(numbers[-1])                
+
+num = 1
+idx = 0
+while idx < len(n):
+    curr = str(num)
+    for c in curr:
+        if idx < len(n) and c == n[idx]:
+            idx += 1
+    num += 1
+
+print(num - 1)
